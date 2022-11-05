54a9159b6be23c1b09ad76987f6c2cbad7e01398
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-11-05 11:23:52 +0500

    feat: add nft receiver example

---
+++ contracts/NFTReceiver.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import { ERC721TokenReceiver } from "./ERC721.sol";
+
+contract NFTReceiver is ERC721TokenReceiver {
+    // nft logic
+    // now this contract can receive nfts
+}
